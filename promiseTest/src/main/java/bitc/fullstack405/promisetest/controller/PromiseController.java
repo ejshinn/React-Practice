21567cef10c7d80a7761d167dca44631bdd4d702
ejshinn <ejshin.dev@gmail.com>
2024-08-19 18:32:15 +0900

    자바스크립트의 비동기 방식 실행

---
+++ promiseTest/src/main/java/bitc/fullstack405/promisetest/controller/PromiseController.java
@@ -0,0 +1,23 @@
+package bitc.fullstack405.promisetest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/async")
+public class PromiseController {
+
+    @GetMapping("/data1")
+    public String getData1() {
+        return "data1";
+    }
+
+    @ResponseBody
+    @PostMapping("/data1")
+    public Object postData() {
+        return "success";
+    }
+}
