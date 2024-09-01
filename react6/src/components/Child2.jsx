cfd67374b80d462462ea3e3ccf016956a18e1b9e
ejshinn <ejshin.dev@gmail.com>
2024-09-01 18:35:57 +0900

    state 공유하기

---
+++ react6/src/components/Child2.jsx
@@ -0,0 +1,15 @@
+// Child2.jsx
+
+function Child2(props) {
+    return (
+        <div className={'border border-2 border-success rounded-2 m-4 p-4'}>
+            {/* 부모 컴포넌트에서 전달받은 데이터는 모두 props.속성명으로 데이터를 가져옴 */}
+            {/* props로 전달받은 데이터는 읽기 전용 */}
+            <h2>오른쪽 자식 컴포넌트 영역</h2>
+            <h3>첫 번째로 전달받은 값 : {props.data}</h3>
+            <h3>두 번째로 전달받은 값 : {props.data2}</h3>
+        </div>
+    );
+}
+
+export default Child2;
