3cc196fbc82ce82e7b459116526e0875af4a5c96
ejshinn <ejshin.dev@gmail.com>
2025-01-10 01:28:10 +0900

    Header 컴포넌트

---
+++ jwtReact/src/components/layout/Header.jsx
@@ -0,0 +1,13 @@
+
+
+function Header() {
+    return (
+        <header className={'my-4 p-5'}>
+            <div className={'container text-center'}>
+                <h1>Header</h1>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
