9ae99fb2c13ec06e217d65c97c745968cb24c5b9
ejshinn <ejshin.dev@gmail.com>
2025-01-10 01:28:46 +0900

    Footer 컴포넌트

---
+++ jwtReact/src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+    return (
+        <div>
+            <footer className={'container-fluid border-top mt-5 p-5'}>
+                <p className={'lead text-muted text-center'}>made by fullstack405</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer
